b07661cfdbc557d44ffcb0c32da70eae0f47afb6
IndomitusMaha <61577626+IndomitusMaha@users.noreply.github.com>
2021-12-12 21:58:21 +0600

    Add files via upload

--- praktika 2.4.php
+++ praktika 2.4.php
@@ -1,27 +1,24 @@
-<?php
-/*
+<?
+
 function pr ($s) {
-    echo "<pre>";
-    print_r($s);
-    echo "</pre>";
+   echo "<pre>";
+   print_r($s);
+   echo "</pre>";
     
-*/
-
-$arMonth = array();
-
 function SetMonth($days, $dayName){
     if (($days>=28) AND ($days<=31)){
         $arDays = array ("Понедельник", "Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье");
         
-        for ($d; $d < count($arDays); $d++){
-            if ($dayName = $arDays[$d]){
-                $d = $start;
+        for ($d = 0; $d < count($arDays); $d++){
+            if ($dayName == $arDays[$d]){
+                $start = $d;
             }
         }
         for ($j = $start; $j < $days + $start; $j++){
               if (in_array($dayName, $arDays)){
                 $i = $j%7;
-                $arMonth = $arDays[$i];                
+                $arMonth[] = $arDays[$i];  
+                return $arMonth;              
               } 
               else{
                 echo "Введите корретное название дня недели с большой буквы";
@@ -32,10 +29,9 @@ function SetMonth($days, $dayName){
     
     else {
         echo "Введите корректное количество дней в месяце";
-        
     } 
-    
+       
 }
-SetMonth (30, "Понедеик");
-pr($arMonth);
+$arResult = SetMonth(31, "Понедельник");
+pr($arResult);
 ?>
